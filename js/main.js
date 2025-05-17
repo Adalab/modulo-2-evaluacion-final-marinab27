01a77e98e0eb79b15d2dec36adc075f66e01c5ad
marina.boyano.gonzalez@gmail.com <marina@DESKTOP-HBBCQPA.localdomain>
2025-05-17 10:51:11 +0200

    avances en ejercicio

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const productContainer = document.querySelector(".js-product-container");
 let products = [];
 const searchButton = document.querySelector(".js-search-button");
 
+
 function renderProducts(products) {
     for (let product of products) {
         const card = document.createElement("div");
@@ -15,11 +16,12 @@ function renderProducts(products) {
             <img src="${product.image}" alt="${product.title}">
             <p>${product.title}</p>
             <p>Price: $${product.price}</p>
-            <button class="js-add-to-cart" data-id="${product.id}">Comprar</button>
+            <button class="js-add-to-cart" data-id="${product.id}">Comprar</button>            
         `;
         productContainer.appendChild(card);
     }
-
+    let addToCartButton = document.querySelectorAll(".js-add-to-cart");
+    addToCartButton.addEventListener("click", handleAddToCart);
 }
 
 
@@ -38,6 +40,19 @@ const handleClick = (event) => {
 searchButton.addEventListener("click", handleClick);
 
 
+
+const handleAddToCart = (event) => {
+    event.preventDefault();
+    console.log("he hecho click en el botón de comprar");
+
+}
+
+
+
+
+//1) al hacer click: el color del boton cambia y cambia el texto a Eliminar. Opción 1: que haya dos botones y con if else alternarlos. Opción 2: agregarle una clase diferente de css al hacer click.
+
+//2) Hacer que el div vacío que hay en el carro contenga los productos que clickemos. para eso hay que crear un array vacío y meterle los productos que vayan al carro. y pintarlos. 
 
 
 
